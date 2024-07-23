cbe52efb473e44c490813a8a981ca63e0a821971
Ravi <ravibhagat352@gmail.com>
2024-07-23 19:44:21 +0530

    in this commit I have explored about the Constructors

---
+++ src/com/constructors/Constants.java
@@ -0,0 +1,11 @@
+package com.constructors;
+
+public class Constants {
+
+    private Constants(){
+
+    }
+    public static final int NUMBER_OF_MONTH_IN_A_YEAR = 12;
+    public static final int NUMBER_OF_DAYS_IN_WEEK = 7;
+    public static final String MY_NAME = "RAVI";
+}
---
+++ src/com/constructors/Constructors.java
@@ -0,0 +1,12 @@
+package com.constructors;
+
+public class Constructors {
+
+    public static void main(String[] args) {
+        Dog myDog = new Dog("Sandy", 5);
+        System.out.println(myDog);
+        System.out.println(Constants.MY_NAME);
+        System.out.println(Constants.NUMBER_OF_MONTH_IN_A_YEAR);
+        System.out.println(Constants.NUMBER_OF_DAYS_IN_WEEK);
+    }
+}
